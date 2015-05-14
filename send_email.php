54d28e2d87584d0fd87fad3ae0623f9b8f3b0343
AdnaDurakovic <adurakovic4@etf.unsa.ba>
2015-05-14 03:59:33 +0200

    Slanje maila (gmail)
    pokušaj slanja

--- send_email.php
+++ send_email.php
@@ -1,4 +1,41 @@
 <?php
+require("C:\PHPMailer_5.2.0\class.phpmailer.php");
+
+$mail = new PHPMailer();
+
+$mail->IsSMTP();                                      // set mailer to use SMTP
+$mail->Host = "smtp.gmail.com";  // specify main and backup server
+$mail->SMTPSecure = "ssl";
+$mail->Port = 465;
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->Username = "adna.durakovic11@gmail.com";  // SMTP username
+$mail->Password = "fakatmiseneda9111993"; // SMTP password
+
+$mail->From = "adna.durakovic11@gmail.com";
+$mail->FromName = "Adna";
+//$mail->AddAddress("josh@example.net", "Josh Adams");
+$mail->AddAddress("adurakovic4@etf.unsa.ba");                  // name is optional
+//$mail->AddReplyTo("info@example.com", "Information");
+
+$mail->WordWrap = 50;                                 // set word wrap to 50 characters
+//$mail->AddAttachment("/var/tmp/file.tar.gz");         // add attachments
+//$mail->AddAttachment("/tmp/image.jpg", "new.jpg");    // optional name
+$mail->IsHTML(true);                                  // set email format to HTML
+
+$mail->Subject = "Here is the subject";
+$mail->Body    = "This is the HTML message body <b>in bold!</b>";
+$mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+
+if(!$mail->Send())
+{
+   echo "Message could not be sent. <p>";
+   echo "Mailer Error: " . $mail->ErrorInfo;
+   exit;
+}
+
+echo "Message has been sent";
+
+
 /*
   require("C:\sendgrid-php\sendgrid-php.php");
 
@@ -18,7 +55,7 @@
 
   $response=$sendgrid->send($email);
   print_r($response);
-*/
+
 
   ini_set("SMTP", "webmail.etf.unsa.ba");
   ini_set("smtp_port", "25");
@@ -47,6 +84,6 @@ else
 {
   echo "Email nije poslan.";
 }
-
+*/
 
 ?>
