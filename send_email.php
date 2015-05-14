7760f231f8e0a940cee9e8b65998dd7fbbe83fda
AdnaDurakovic <adurakovic4@etf.unsa.ba>
2015-05-14 11:24:44 +0200

    Sendgrid

--- send_email.php
+++ send_email.php
@@ -1,4 +1,24 @@
 <?php
+
+  $name = $_POST['firstname'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['myname'];
+
+  require("sendgrid-php/sendgrid-php.php");
+
+  $service_plan_id = "sendgrid_e3f7b";
+  $account_info = json_decode(getenv($service_plan_id), true);
+
+  $sendgrid = new SendGrid("aadurakovic4285", "1DvaTri!");
+
+  $email = new SendGrid\Email();
+
+  $email->addTo("adurakovic4@etf.unsa.ba")->addCc("adna.durakovic11@gmail.com")->setFrom($visitor_email)->setSubject("Submitovana forma - Zeljeznice SoulTrain")->setText($message);
+
+  $sendgrid->send($email);
+  echo '<script>alert("Zahvaljujemo se što ste nas kontaktirali.")</script>';
+
+/*
 header('Content-Type: text/html; charset=utf-8');
 
 
@@ -26,6 +46,6 @@ else
 {
   echo "Email nije poslan.";
 }
-
+*/
 
 ?>
