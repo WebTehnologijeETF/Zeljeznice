8e7d4c10059199e302bec84a820b6fa4e39f8b53
AdnaDurakovic <adurakovic4@etf.unsa.ba>
2015-05-14 04:22:20 +0200

    Komit stoti
    Kaže da radi...

--- send_email.php
+++ send_email.php
@@ -1,28 +1,6 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 
-/*
-  require("C:\sendgrid-php\sendgrid-php.php");
-
-  // get account info from OpenShift environment variable
-  $service_plan_id = "sendgrid_e3f7b"; // your OpenShift Service Plan ID
-  $account_info = json_decode(getenv($service_plan_id), true);
-
-  $sendgrid = new SendGrid($account_info['username'], $account_info['password']);
-  $email    = new SendGrid\Email();
-
-  $email->addTo("adurakovic4@etf.unsa.ba")
-        ->setFrom("adna.durakovic11@gmail.com")
-        ->setSubject("Sending with SendGrid is Fun")
-        ->setText('Hi I have sent you a mail!')
-        ->setHtml('<strong>and easy to do anywhere, even with PHP</strong>');
-        
-
-  $response=$sendgrid->send($email);
-  print_r($response);
-
-*/
-
-  //ini_set("smtp_port", "25");
 
   $name = $_POST['firstname'];
   $visitor_email = $_POST['email'];
@@ -48,6 +26,6 @@ else
 {
   echo "Email nije poslan.";
 }
-*/
+
 
 ?>
